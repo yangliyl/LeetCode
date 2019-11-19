24a9d8d74a6f646bbdced6af343aaaf6e2437881
yangliyl <yangli_yl@qq.com>
2019-11-19 10:42:05 +0800

    add 12

--- 0012.go
+++ 0012.go
@@ -1,15 +1,3 @@
-package main
-
-import "bytes"
-
-import "fmt"
-
-import "strings"
-
-func main() {
-	fmt.Println(intToRoman(3))
-}
-
 func intToRoman(num int) string {
 	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	romans := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
