23856475346b403e0aad4e0ba5d143aabad387cd
yangliyl <yangli_yl@qq.com>
2019-11-19 10:12:46 +0800

    add 12

---
+++ 0012.go
@@ -0,0 +1,25 @@
+package main
+
+import "bytes"
+
+import "fmt"
+
+import "strings"
+
+func main() {
+	fmt.Println(intToRoman(3))
+}
+
+func intToRoman(num int) string {
+	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	romans := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+	buf := bytes.Buffer{}
+	for i := 0; i < len(nums); i++ {
+		if num >= nums[i] {
+			buf.WriteString(strings.Repeat(romans[i], num/nums[i]))
+			num %= nums[i]
+		}
+	}
+	return buf.String()
+}
