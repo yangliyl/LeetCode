96f433f2c978a378a9fbbfdb57ce6aa30f267954
yangliyl <yangli_yl@qq.com>
2019-12-04 09:13:29 +0800

    add 26

---
+++ 0026.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+	i := 0
+	for j := 1 ; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			nums[i+1] = nums[j]
+			i++
+		}
+	}
+	return i + 1
+}
